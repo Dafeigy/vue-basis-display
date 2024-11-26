6ac330a4ba110d7c34f1d4f80d405f9d7297cdb0
zclai <1070642765@qq.com>
2024-11-26 13:55:58 +0800

    <Feat>: Backend return with contract expired time.

--- app.py
+++ app.py
@@ -2,36 +2,58 @@ from flask import Flask, jsonify, request
 import datetime
 from flask_cors import CORS
 import random
-
+from datetime import datetime
 app = Flask(__name__)
 CORS(app)
 
 def fake_generator(date):
-    now = datetime.datetime.now()
+    now = datetime.now()
     return_time = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    # 定义特定日期
+    if date == "2412":
+        target_date = datetime(2024, 12, 20)
+    elif date == "2501":
+        target_date = datetime(2025, 1, 17)
+    elif date == "2503":
+        target_date = datetime(2025, 3, 21)
+    elif date == "2506":
+        target_date = datetime(2025, 6, 20)
+    else:
+        print("Invalid Contract date")
+        return {}
+
+    # 获取当前日期
+    current_date = datetime.now()
+
+    # 计算日期差
+    date_difference = target_date - current_date
+
+    # 输出天数差
+    days_difference = date_difference.days
     contracts_data = {
         f"IH_{date}":{
             "buy_value":random.randint(2500,2700),
             "sell_value":random.randint(2500,2700)
         },
         f"IF_{date}":{
-            "buy_value":random.randint(2500,2700),
-            "sell_value":random.randint(2500,2700),
+            "buy_value":random.randint(3900,4100),
+            "sell_value":random.randint(3900,4100),
         },
         f"IC_{date}": {
-            "buy_value":random.randint(2500,2700),
-            "sell_value":random.randint(2500,2700),
+            "buy_value":random.randint(5900,6100),
+            "sell_value":random.randint(5900,6100),
         },
         f"IM_{date}": {
-            "buy_value":random.randint(2500,2700),
-            "sell_value":random.randint(2500,2700),
+            "buy_value":random.randint(6000,6200),
+            "sell_value":random.randint(6000,6200),
         }
     }
     
     
     res = {
         "data": contracts_data,
-        "time": return_time
+        "time": return_time,
+        "expired": days_difference
     }
     return res
 @app.route("/get-etf-data")
@@ -43,7 +65,7 @@ def etf_data_generator():
         "sh905_value": random.randint(5900,6100),
         "sh852_value": random.randint(6000,6200)
     }
-    now = datetime.datetime.now()
+    now = datetime.now()
     return_time = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
     return jsonify({
         'data':etf_data,
@@ -57,6 +79,7 @@ def get_contracts_data():
     response_origin_raw = {}
     for each in contracts:
         response_origin_raw[each] = fake_generator(each)
+
     # print(response_origin_raw)
     # print("\n")
     return jsonify(response_origin_raw)
